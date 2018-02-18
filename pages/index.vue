3bbba58982a0e9e335a2f9ba54f6cc14b9f60b6f
Iskander Broere <iskanderbroere@gmail.com>
2018-02-18 22:28:10 +0100

    i like less things and started to like dotted borders

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,19 @@
     <nav>
       <h1><em>iskander</em><strong>broere</strong></h1>
     </nav>
-    <main class="grid">
+    <main>
       <section>
         <h2>hi, we're in an elevator, this is my pitch, i</h2>
         <dl>
-          <dt>write</dt>
+          <dt><h3>write</h3></dt>
           <dd>modern full-stack web applications</dd>
-          <dt>use</dt>
+          <dt><h3>use</h3></dt>
           <dd>JS - HTML - CSS - GraphQL - Markdown - MongoDB - PostgreSQL - Vue - React - Express - Mongoose - Sequelize - PostCSS - SCSS - Bootstrap - Webpack - Yarn - NPM - ESLint - Stylelint - Prettier - VSCode - Hyper - Netlify - GitHub</dd>
-          <dt>like</dt>
-          <dd>PWAs - CI - TDD - Static Hosting - Decoupling Applications - Microservices</dd>
+          <dt><h3>like</h3></dt>
+          <dd>PWAs - CI - Static Hosting - Decoupled Applications</dd>
         </dl>
         <hr>
-        <p><em>Eager to learn, always seeking to improve, perfectionistic yet practical, that is who I am.</em> Still reading? Hit me up at <a href="mailto:iskanderbroere@gmail.com">iskanderbroere@gmail.com</a></p>
+        <p><em>Eager to learn, always seeking to improve, perfectionistic yet practical, that is who I am.</em> Still reading? Hit me up! <a href="mailto:iskanderbroere@gmail.com">iskanderbroere@gmail.com</a></p>
       </section>
     </main>
   </div>
