10983db31012e00ddac36079412d00f771994177
Iskander Broere <iskanderbroere@gmail.com>
2018-02-19 12:14:45 +0100

    pwa meta tags icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,30 @@ module.exports = {
   head: {
     title: "Iskander Broere - modern full-stack web applications",
     meta: [
-      { charset: "utf-8" },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1, shrink-to-fit=no"
-      },
       { "http-equiv": "x-ua-compatible", content: "ie=edge" },
       { name: "google", content: "nositelinkssearchbox" },
       {
         name: "google-site-verification",
         content: "rzvyF6AxI29JhBHq0l6dzvaH_s4-Qq9AaSILG2VFuAA"
-      },
-      {
-        hid: "description",
-        name: "description",
-        content:
-          "JS - HTML - CSS - GraphQL - Vue - React - Express - Bootstrap - Webpack - Yarn - NPM - Netlify - PWAs - CI - Static Hosting - Decoupled Applications"
       }
     ]
   },
-  modules: ["@nuxtjs/icon"],
+  modules: ["@nuxtjs/pwa"],
+  meta: {
+    viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+    name: "Iskander Broere - modern full-stack web applications",
+    description:
+      "JS - HTML - CSS - GraphQL - Vue - React - Express - Bootstrap - Webpack - Yarn - NPM - Netlify - PWAs - CI - Static Hosting - Decoupled Applications",
+    lang: "en",
+    mobileApp: false
+  },
+  manifest: {
+    name: "Iskander Broere - modern full-stack web applications",
+    short_name: "Iskander Broere",
+    description:
+      "JS - HTML - CSS - GraphQL - Vue - React - Express - Bootstrap - Webpack - Yarn - NPM - Netlify - PWAs - CI - Static Hosting - Decoupled Applications",
+    background_color: "#f9f9f9"
+  },
   loading: { color: "#ced4da" },
   css: ["~/assets/main.css"]
 }
