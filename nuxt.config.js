705a6321c7c571b9c6c8a9e18d2da86d8ed55891
Iskander Broere <iskanderbroere@gmail.com>
2018-02-19 10:13:45 +0100

    robots meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  htmlAttrs: {
+    lang: "en"
+  },
   mode: "universal",
   head: {
-    title: "Iskander Broere",
+    title: "Iskander Broere - modern full-stack web applications",
     meta: [
       { charset: "utf-8" },
       {
@@ -9,7 +12,17 @@ module.exports = {
         content: "width=device-width, initial-scale=1, shrink-to-fit=no"
       },
       { "http-equiv": "x-ua-compatible", content: "ie=edge" },
-      { hid: "description", name: "description", content: "Personal website" }
+      { name: "google", content: "nositelinkssearchbox" },
+      {
+        name: "google-site-verification",
+        content: "rzvyF6AxI29JhBHq0l6dzvaH_s4-Qq9AaSILG2VFuAA"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "JS - HTML - CSS - GraphQL - Vue - React - Express - Bootstrap - Webpack - Yarn - NPM - Netlify - PWAs - CI - Static Hosting - Decoupled Applications"
+      }
     ]
   },
   loading: { color: "#ced4da" },
