721349050a9b7452f5f5f0b1a8023da5232321f2
Iskander Broere <iskanderbroere@gmail.com>
2018-03-14 13:55:46 +0100

    more projects added and h3 styling

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,10 +2,17 @@
   <main>
     <section>
       <h2>Projects</h2>
+      <h3>In production</h3>
       <ul>
-        <li>M&aacute;ty&aacute;s Bittenbinder - <a href="https://mbittenbinder.com" rel="noopener" target="_blank">mbittenbinder.com</a> - <small>Vue - Netlify - Contentful</small></li>
-        <li>Jolien Wesselink - <a href="https://jolienwesselink.nl" rel="noopener" target="_blank">jolienwesselink.nl</a> - <small>Vue - Netlify - Contentful</small></li>
-        <li>DepoClaim - <a href="https://depoclaimbv.netlify.com" rel="noopener" target="_blank">DepoClaim.com</a> - <small>Vue - Netlify - Contentful</small></li>
+        <li>Personal website - <small>Vue - Netlify - SSR</small></li>
+        <li>M&aacute;ty&aacute;s Bittenbinder - <a href="https://mbittenbinder.com" rel="noopener" target="_blank">mbittenbinder.com</a> - <small>Vue - Netlify - SSR - Contentful - Bootstrap 4</small></li>
+        <li>Jolien Wesselink - <a href="https://jolienwesselink.nl" rel="noopener" target="_blank">jolienwesselink.nl</a> - <small>Vue - Netlify - SSR - Contentful - Bootstrap 4</small></li>
+      </ul>
+      <h3>In development</h3>
+      <ul style="margin-bottom: 0;">
+        <li>DepoClaim - <a href="https://depoclaimbv.netlify.com" rel="noopener" target="_blank">DepoClaim.com</a> - <small>Vue - Netlify - SSR - Contentful - Bootstrap 4</small></li>
+        <li>Vue Apollo GraphQL - <a href="https://github.com/iskanderbroere/gqvue" rel="noopener" target="_blank">GitHub repository</a> - <small>Vue - Apollo GraphQL</small></li>
+        <li>IPShares - <a href="https://github.com/IPSharesSam/IPShares" rel="noopener" target="_blank">GitHub repository</a> - <small>React - Material Design - Express - Heroku</small></li>
       </ul>
     </section>
   </main>
