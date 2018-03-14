cda4e589ac58ae1187f27ed41375b797ea50a370
Iskander Broere <iskanderbroere@gmail.com>
2018-03-14 13:02:08 +0100

    projects

---
+++ pages/projects/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        <li>M&aacute;ty&aacute;s Bittenbinder - <a href="https://mbittenbinder.com" rel="noopener" target="_blank">mbittenbinder.com</a> - <small>Vue - Netlify - Contentful</small></li>
+        <li>Jolien Wesselink - <a href="https://jolienwesselink.nl" rel="noopener" target="_blank">jolienwesselink.nl</a> - <small>Vue - Netlify - Contentful</small></li>
+        <li>DepoClaim - <a href="https://depoclaimbv.netlify.com" rel="noopener" target="_blank">DepoClaim.com</a> - <small>Vue - Netlify - Contentful</small></li>
+      </ul>
+    </section>
+  </main>
+</template>
