fdee7795da2664f6cbac00dfeec755a61cc6cb12
Iskander Broere <iskanderbroere@Iskanders-MacBook-Pro.local>
2018-05-02 22:38:41 +0200

    depoclaim prod

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -5,12 +5,12 @@
       <h3>In production</h3>
       <ul>
         <li>Personal website - <small>Vue.js - Nuxt - Netlify - SSR - PostCSS</small></li>
+        <li>DepoClaim - <a href="https://depoclaim.com" rel="noopener noreferrer" target="_blank">DepoClaim.com</a> - <small>Vue.js - Nuxt - Netlify - SSR - Contentful - Bootstrap 4 - Markdown</small></li>
         <li>M&aacute;ty&aacute;s Bittenbinder - <a href="https://mbittenbinder.com" rel="noopener noreferrer" target="_blank">mbittenbinder.com</a> - <small>Vue.js - Nuxt - Netlify - SSR - Contentful - Bootstrap 4</small></li>
         <li>Jolien Wesselink - <a href="https://jolienwesselink.nl" rel="noopener noreferrer" target="_blank">jolienwesselink.nl</a> - <small>Vue.js - Nuxt - Netlify - SSR - Contentful - Bootstrap 4 - Markdown</small></li>
       </ul>
       <h3>In development</h3>
       <ul style="margin-bottom: 0;">
-        <li>DepoClaim - <a href="https://depoclaimbv.netlify.com" rel="noopener noreferrer" target="_blank">DepoClaim.com</a> - <small>Vue.js - Nuxt - Netlify - SSR - Contentful - Bootstrap 4 - Markdown</small></li>
         <li>Vue.js Apollo GraphQL - <a href="https://github.com/iskanderbroere/gqvue" rel="noopener noreferrer" target="_blank">GitHub repository</a> - <small>Vue.js - Nuxt - Apollo GraphQL - GraphQL Yoga - Material Design</small></li>
         <li>Adonuxt - <a href="https://github.com/iskanderbroere/adonuxt" rel="noopener noreferrer" target="_blank">GitHub repository</a> - <small>Vue.js - Nuxt - AdonisJS - PostgreSQL - Material Design</small></li>
         <li>IPShares - <a href="https://github.com/IPSharesSam/IPShares" rel="noopener noreferrer" target="_blank">GitHub repository</a> - <small>React - Express - MongoDB - Heroku - Material Design</small></li>
