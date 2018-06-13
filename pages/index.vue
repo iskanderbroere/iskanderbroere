b8c197ed7c626ed7d5edd76b03d779fd07eb7cc8
Iskander Broere <iskanderbroere@iskanders-mbp.home>
2018-06-13 21:50:51 +0200

    upgrades

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,11 @@
       <h2>hi, we're in an <span class="green">elevator</span>, this is my <span class="green">pitch</span>, i</h2>
       <dl>
         <dt>write</dt>
-        <dd>modern full-stack web applications</dd>
+        <dd>modern full-stack web applications @ <a href="https://frontmen.nl" target="_blank" rel="noopener noreferrer">Frontmen</a></dd>
         <dt>use</dt>
-        <dd>JS - HTML - CSS - GraphQL - Markdown - MongoDB - PostgreSQL - Vue.js - React - Express - Mongoose - Sequelize - PostCSS - SCSS - Bootstrap 4 - Apollo GraphQL - Webpack - Yarn - NPM - Prettier - ESLint - Stylelint - VSCode - Netlify - GitHub</dd>
+        <dd>JavaScript - HTML - CSS - GraphQL - Markdown - MongoDB - PostgreSQL - Vue.js 💚 - React - Express - PostCSS - SCSS - CSSinJS - Material Design - Bootstrap 4 - Apollo GraphQL ⭐ - Webpack - Yarn - NPM - Prettier - ESLint - Stylelint - VSCode - Netlify - GitHub</dd>
         <dt>like</dt>
-        <dd>PWAs - CI - Static Hosting - Decoupled Applications</dd>
+        <dd>Performance 🚀 - PWAs - CI - Static Hosting - Decoupled Applications - AWS</dd>
       </dl>
       <hr>
       <p><em>Eager to learn, always seeking to improve, perfectionistic yet practical, that is who I am.</em> Still reading? Hit me up! </p>
