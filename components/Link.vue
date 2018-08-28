dc210954cd021aabc3e627573dc4edfc397572ba
Iskander Broere <iskanderbroere@192.168.2.13>
2018-08-28 19:20:22 +0200

    upgrades

---
+++ components/Link.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    href="https://www.linkedin.com/in/iskander-broere-51065615b/"
+    target="_blank"
+    rel="noopener noreferrer"
+    @click="toLinkedIn">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
