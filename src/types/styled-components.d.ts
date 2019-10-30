89efbe4171d99a02d14247572523e2dbaf720e51
Iskander Broere <iskanderbroere@gmail.com>
2019-10-30 21:13:52 +0100

    feat: improve theme typings

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,27 +1,9 @@
 import {} from "styled-components/cssprop"
 // import original module declarations
 import "styled-components"
+import { Theme } from "../styles/theme"
 
 // and extend them!
 declare module "styled-components" {
-  export interface DefaultTheme {
-    fonts: {
-      body: string
-    }
-    colors: {
-      primary: {
-        main: string
-        dark: string
-      }
-      grays: [string, string, string]
-    }
-    breakpoints: {
-      xs: string
-      sm: string
-      md: string
-      lg: string
-      xl: string
-      xxl?: string
-    }
-  }
+  export interface DefaultTheme extends Theme
 }
