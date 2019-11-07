d520d8172af8084a3d7f7ea91f748eb663507d63
Iskander Broere <iskanderbroere@gmail.com>
2019-11-07 20:16:09 +0100

    chore: clean up comment

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,9 +1,7 @@
 import {} from "styled-components/cssprop"
-// import original module declarations
 import "styled-components"
 import { Theme } from "../styles/theme"
 
-// and extend them!
 declare module "styled-components" {
   export interface DefaultTheme extends Theme
 }
