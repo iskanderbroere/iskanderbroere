17a3360130be293930ffc5debda17f8eef837d75
Iskander Broere <iskanderbroere@gmail.com>
2020-04-29 23:49:52 +0200

    fix: lint tests

--- cypress/integration/homepage.spec.ts
+++ cypress/integration/homepage.spec.ts
@@ -1,18 +1,11 @@
-describe("Homepage", function() {
-  before(function() {
+describe("Homepage", function () {
+  before(function () {
     cy.visit("/")
   })
-  it("renders nav title", function() {
+  it("renders nav title", function () {
     cy.findByText("iskanderbroere").should("exist")
   })
-  it("renders mailto link", function() {
-    cy.findByText("iskanderbroere@gmail.com").should(
-      "have.prop",
-      "href",
-      "mailto:iskanderbroere@gmail.com"
-    )
-  })
-  it("renders work link", function() {
+  it("renders work link", function () {
     cy.findByText("Frontmen")
       .should("have.prop", "href", "https://www.frontmen.nl/")
       .should("have.prop", "target", "_blank")
