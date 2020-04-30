abb8f672c4358d476c0cb510fb3ebbe6cae50aa6
Iskander Broere <iskanderbroere@gmail.com>
2020-04-30 22:08:03 +0200

    fix: add missing meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,23 @@ import React from "react"
 import { AppProps } from "next/app"
 import { Layout } from "../components/Layout"
 import "../styles/global.scss"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          name="viewport"
+        />
+      </Head>
+      <Layout>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
