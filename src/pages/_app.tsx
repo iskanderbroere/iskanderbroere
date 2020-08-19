bdb9e94ff55a87d1323e446b12303959e99c0094
Iskander Broere <iskanderbroere@gmail.com>
2020-08-19 21:47:27 +0200

    fix: meta tags error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,28 @@
 import React from "react"
+import Head from "next/head"
+import { useRouter } from "next/router"
 import { AppProps } from "next/app"
 import { Layout } from "../components/Layout"
 import "../styles/global.scss"
-import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
   return (
     <>
       <Head>
+        <title>Iskander Broere</title>
         <meta charSet="utf-8" />
         <meta
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
           name="viewport"
+        />
+        <link
+          href={`https://iskanderbroere.nl${router.pathname}`}
+          rel="canonical"
+        />
+        <meta
+          content="I write modern web applications @ Frontmen, am eager to learn, always seeking to improve, perfectionistic yet practical"
+          name="description"
         />
       </Head>
       <Layout>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head"
 import React from "react"
+import Head from "next/head"
 import { uniq } from "ramda"
 import { ExternalLink } from "../components/Link"
 import { ListItem } from "../components/ListItem"
