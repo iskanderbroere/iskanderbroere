4da2e15780344bba6210c153f26c88f0dc410687
Iskander Broere <iskanderbroere@gmail.com>
2020-09-06 22:54:31 +0200

    chore: remove comments

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -9,23 +9,11 @@ export function TopNavigation({ children }: TopNavigationProps) {
   return (
     <nav
       className={clsx([
-        // "flex",
-        // "items-center",
-        // "justify-between",
         "grid",
         "grid-cols-3",
         "grid-rows-2",
         "gap-2",
-        // "py-4",
-        // "px-0",
-        // "border-t-2",
-        // "border-l-2",
-        // "border-gray-700",
-        // "border-dotted",
         "my-4",
-        // "border-b-2",
-        // "border-dotted",
-        // "border-gray-700",
         "w-full",
         "leading-none",
       ])}
