0ef8e026f750d0378b5cd50e3a65222ad9f03c25
Iskander Broere <iskanderbroere@gmail.com>
2024-02-16 22:07:57 +0100

    feat: enhanced img

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,13 @@
 </svelte:head>
 
 <div
-	class="text-gunmetal dark:text-silver flex h-dvh flex-col items-center justify-center gap-1 px-2 md:gap-1.5 lg:gap-2"
+	class="flex h-dvh flex-col items-center justify-center gap-1 px-2 text-gunmetal md:gap-1.5 lg:gap-2 dark:text-silver"
 >
 	<h1 class="text-center text-xl md:text-2xl lg:text-4xl">iskander broere</h1>
 	<p class="text-center font-extrabold md:text-xl lg:text-2xl">webdev</p>
-	<img src={bg} alt="" class="box-content h-auto w-64 rounded-full md:w-72 lg:w-96" />
+	<enhanced:img
+		src="../lib/assets/bg.jpg"
+		alt=""
+		class="box-content h-auto w-64 rounded-full md:w-72 lg:w-96"
+	/>
 </div>
